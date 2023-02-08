89dbdcb77d9eda5b0477cfbb3511643260c3b8d4
Dolores <104917935+d010r3s@users.noreply.github.com>
2023-02-08 17:43:32 +0300

    update

--- main.c
+++ main.c
@@ -1,89 +1,15 @@
-#include "library.h"
 
+//#include "library.h"
+#include <stdio.h>
 
-float input(const char ch[]) {
-    //double pie = 3.1415926;
-
-    int i = 3, j = 0;
-    char temp[10];
-    while (ch[i] != '\0') {
-        if (ch[i] == ' ') {
-            i++;
-            break;
-        }
-        i++;
-    }
-    while (ch[i] != '\0') {
-        temp[j] = ch[i];
-        j++;
-        i++;
-    }
-    return dle(atoi(temp));
+int main(void) {
+    //printf("\nEnter the input as 'sin 90' and 'asin 0.5' for inverse and 'exit' to exit:\n");
+    //char a[N];
+    // scanf("%s",&a);
+    // getc(stdin);
+    //while(getc(stdin));
+    //super_loop(*a);
+   // printf("%s",calc());
+   //calc();
 }
 
-void super_loop(const char * ch) {
-        char ch[20];
-        float res;
-        int sucks = 0;
-        while (1) {
-            printf("\n>");
-           // gets(ch);
-            switch (ch[0]) {
-                case 'e':
-                    res = exp(input(ch));
-                    break;
-                case 'l':
-                    switch (ch[1]) {
-                        case 'n':
-                            res = log(input(ch));
-                            break;
-                        case 'o':
-                            res = log10(input(ch));
-                            break;
-                    }
-                    break;
-                case 's':
-                    switch (ch[1]) {
-                        case 'i':
-                            res = sinf(input(ch));
-                            break;
-                        case 'q':
-                            res = sqrt(input(ch));
-                            break;
-                    }
-                    break;
-                case 'c':
-                    res = cosf(input(ch));
-                    break;
-                case 't':
-                    res = tanf(input(ch));
-                    break;
-                case 'a':
-                    switch (ch[1]) {
-                        case 'b':
-                            res = abs(input(ch));
-                            break;
-                        case 's':
-                            res = asinf(input(ch));
-                            break;
-                        case 'c':
-                            res = acosf(input(ch));
-                            break;
-                        case 't':
-                            res = atanf(input(ch));
-                            break;
-                    }
-                    break;
-                case 'q':
-                    return;
-                default:
-                    sucks = 1;
-                    printf("Invalid Input! Retry\n");
-            }
-            if (!sucks) {
-                printf("Output: %f\n", res);
-            } else {
-                sucks = 0;
-            }
-        }
-}
